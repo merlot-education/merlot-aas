fe28d31c422f22882a1396387908f055c7075a4e
dsukhoro <dsukhoro@spbnb-prc3914.t-systems.ru>
2022-02-08 09:47:31 +0300

    project setup

---
+++ service/src/main/java/eu/gaiax/difs/aas/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+/*-
+ * ---license-start
+ * EU Digital Green Certificate Gateway Service / dgc-gateway
+ * ---
+ * Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
+package eu.gaiax.difs.aas.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * The Spring Security config.
+ */
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfig {
+
+    /**
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.requestMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeRequests((requests) -> requests.anyRequest().permitAll()); // hasRole("ENDPOINT_ADMIN"));
+        return http.build();
+    }
+
+}
