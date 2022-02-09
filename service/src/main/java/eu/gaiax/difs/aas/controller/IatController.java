402799c54437dcbe3dd069d27265074086c154af
nemesviktor <nemes.vik@gmail.com>
2022-02-09 17:03:54 +0100

    New IAT controller with controller tests

---
+++ service/src/main/java/eu/gaiax/difs/aas/controller/IatController.java
@@ -0,0 +1,9 @@
+package eu.gaiax.difs.aas.controller;
+
+import eu.gaiax.difs.aas.generated.controller.IatControllerApiDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IatController implements IatControllerApiDelegate {
+    // TODO: override delegated methods eg. link them with business logic
+}
---
+++ service/src/test/java/eu/gaiax/difs/aas/controller/IatControllerTest.java
@@ -0,0 +1,92 @@
+package eu.gaiax.difs.aas.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.gaiax.difs.aas.generated.controller.IatControllerApiController;
+import eu.gaiax.difs.aas.generated.model.AccessRequest;
+import eu.gaiax.difs.aas.generated.model.ServiceAccessScope;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(IatControllerApiController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class IatControllerTest {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void getIatRequest_missingRequestId_405() throws Exception {
+        mockMvc.perform(
+                        get("/clients/iat/request/"))
+                .andExpect(status().isMethodNotAllowed());
+    }
+    
+    @Test
+    void postIatRequest_missingAccessRequest_400() throws Exception {
+        mockMvc.perform(
+                        post("/clients/iat/request")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(new AccessRequest())))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void postRequest_missingServiceAccessScope_400() throws Exception {
+        mockMvc.perform(
+                        post("/clients/iat/request")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(new AccessRequest().subject("testSubject"))))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void postRequest_missingSubject_400() throws Exception {
+        mockMvc.perform(
+                        post("/clients/iat/request")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(new AccessRequest()
+                                        ._object(
+                                                new ServiceAccessScope().scope("testScope").did("testDid")
+                                        )
+                                )))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void postRequest_missingSubjectDid_400() throws Exception {
+        mockMvc.perform(
+                        post("/clients/iat/request")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(new AccessRequest()
+                                        .subject("testSubject")
+                                        ._object(
+                                                new ServiceAccessScope().scope("testScope")
+                                        )
+                                )))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void postRequest_missingSubjectScope_400() throws Exception {
+        mockMvc.perform(
+                        post("/clients/iat/request")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(new AccessRequest()
+                                        .subject("testSubject")
+                                        ._object(
+                                                new ServiceAccessScope().did("testDid")
+                                        )
+                                )))
+                .andExpect(status().isBadRequest());
+    }
+}
