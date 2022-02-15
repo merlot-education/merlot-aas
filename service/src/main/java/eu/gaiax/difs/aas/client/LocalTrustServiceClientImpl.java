aff9bcb2ff530068f82cac3a46f132e56da800a1
nemesviktor <nemes.vik@gmail.com>
2022-02-15 14:54:12 +0100

    Local trust service implementation

--- service/src/main/java/eu/gaiax/difs/aas/client/LocalTrustServiceClientImpl.java
+++ service/src/main/java/eu/gaiax/difs/aas/client/LocalTrustServiceClientImpl.java
@@ -1,18 +1,19 @@
 package eu.gaiax.difs.aas.client;
 
+import eu.gaiax.difs.aas.properties.LocalTrustServiceClientProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Component
 public class LocalTrustServiceClientImpl implements TrustServiceClient {
 
-    private Map<String, Object> config;
+    @Autowired
+    private LocalTrustServiceClientProperties config;
 
     @Override
     public Map<String, Object> evaluate(String policyname, Map<String, Object> bodyParams) {
-        // check params passed and return response, dependent on the params and local config
-        return Collections.emptyMap();
+        return config.getPolicyMocks().get(policyname);
     }
 }
