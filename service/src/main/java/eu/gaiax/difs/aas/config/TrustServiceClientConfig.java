5a77b9fa63f08a9b2686044edff866c1a077f9da
nemesviktor <nemes.vik@gmail.com>
2022-03-02 15:54:34 +0100

    logging errors, refactoring

--- service/src/main/java/eu/gaiax/difs/aas/config/TrustServiceClientConfig.java
+++ service/src/main/java/eu/gaiax/difs/aas/config/TrustServiceClientConfig.java
@@ -3,7 +3,6 @@ package eu.gaiax.difs.aas.config;
 import eu.gaiax.difs.aas.client.LocalTrustServiceClientImpl;
 import eu.gaiax.difs.aas.client.RestTrustServiceClientImpl;
 import eu.gaiax.difs.aas.client.TrustServiceClient;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
