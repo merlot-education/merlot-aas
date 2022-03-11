88dbf16805a3c79388b9bad73d8d7147332b7ae0
gerrigom <giancarlo.errigo-mattos@t-systems.com>
2022-03-11 14:36:51 +0100

    Language and style fixes

--- service/src/main/resources/static/js/translate.js
+++ service/src/main/resources/static/js/translate.js
@@ -2,31 +2,41 @@
 function translate(language) {
     let displayLanguage = document.getElementById("display-language")
     let greeting = document.getElementById("greeting")
+    let signInButton = document.getElementById("sign-in-button")
+    let qrBrowserButton = document.getElementById("scan-qr-browser-button")
     let backButton = document.getElementById("back-to-standard-login-button")
 
     switch (language) {
         case "English":
             displayLanguage.innerText = "English"
             greeting.innerText = "Scan QR code with smartphone"
-            backButton.setAttribute("value", "Login via Browser")
+            signInButton.setAttribute("value", "Sign In")
+            qrBrowserButton.setAttribute("value", "Scan QR code with browser")
+            backButton.setAttribute("value", "Back to Standard Login")
             break
 
         case "Deutsch":
             displayLanguage.innerText = "Deutsch"
             greeting.innerText = "QR-Code mit Smartphone scannen"
-            backButton.setAttribute("value", "Anmeldung über Browser")
+            signInButton.setAttribute("value", "Anmelden")
+            qrBrowserButton.setAttribute("value", "QR-Code mit dem Browser scannen")
+            backButton.setAttribute("value", "Zurück zu Standard Login")
             break
 
         case "Français":
             displayLanguage.innerText = "Français"
             greeting.innerText = "Scanner le code QR avec un smartphone"
-            backButton.setAttribute("value", "Connexion via le navigateur")
+            signInButton.setAttribute("value", "Connexion")
+            qrBrowserButton.setAttribute("value", "Scanner le code QR avec un navigateur")
+            backButton.setAttribute("value", "Retour à Connexion standard")
             break
 
         default:
             displayLanguage.innerText = "English"
             greeting.innerText = "Scan QR code with smartphone"
-            backButton.setAttribute("value", "Login via Browser")
+            signInButton.setAttribute("value", "Sign In")
+            qrBrowserButton.setAttribute("value", "Scan QR code with browser")
+            backButton.setAttribute("value", "Back to Standard Login")
             break
 
     }
