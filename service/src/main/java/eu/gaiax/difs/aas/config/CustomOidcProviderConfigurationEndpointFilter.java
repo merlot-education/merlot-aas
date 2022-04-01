4a31da02c438050c2272449548537d4f6ce620bb
Simon Javorsky <Simon.Javorsky@external.t-systems.com>
2022-04-01 10:36:06 +0200

    update OIDC discovery endpoint, remove unsupported HS256 algorithm

--- service/src/main/java/eu/gaiax/difs/aas/config/CustomOidcProviderConfigurationEndpointFilter.java
+++ service/src/main/java/eu/gaiax/difs/aas/config/CustomOidcProviderConfigurationEndpointFilter.java
@@ -95,12 +95,7 @@ public final class CustomOidcProviderConfigurationEndpointFilter extends OncePer
     }
 
     private Consumer<List<String>> clientAuthenticationMethods() {
-        return (authenticationMethods) -> {
-            authenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_BASIC.getValue());
-            authenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_POST.getValue());
-            authenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_JWT.getValue());
-            authenticationMethods.add(ClientAuthenticationMethod.PRIVATE_KEY_JWT.getValue());
-        };
+        return (authenticationMethods) -> authenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_BASIC.getValue());
     }
 
     private Consumer<List<String>> oidcScopes() {
@@ -120,15 +115,12 @@ public final class CustomOidcProviderConfigurationEndpointFilter extends OncePer
     }
 
     private Consumer<List<String>> signingAlgorithms() {
-        return (signingAlgorithms) -> {
-            signingAlgorithms.add(SignatureAlgorithm.RS256.getName());
-            signingAlgorithms.add("HS256");
-        };
+        return (signingAlgorithms) -> signingAlgorithms.add(SignatureAlgorithm.RS256.getName());
     }
 
     private Consumer<Map<String, Object>> claims() {
         return (claims) -> {
-            claims.put("userinfo_signing_alg_values_supported", List.of("RS256", "HS256"));
+            claims.put("userinfo_signing_alg_values_supported", List.of("RS256"));
             claims.put("display_values_supported", List.of("page", "popup"));
             claims.put("claims_supported", List.of(
                     "sub",
