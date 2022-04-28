6d8e5d82b820d1bd3eae4f841b7f525554a2e7bc
Simon Javorsky <Simon.Javorsky@external.t-systems.com>
2022-04-28 11:05:38 +0200

    added QR check

--- service/src/test/java/eu/gaiax/difs/aas/controller/AuthenticationFlowTest.java
+++ service/src/test/java/eu/gaiax/difs/aas/controller/AuthenticationFlowTest.java
@@ -12,9 +12,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
 import eu.gaiax.difs.aas.client.LocalTrustServiceClientImpl;
 import eu.gaiax.difs.aas.client.TrustServiceClient;
 import eu.gaiax.difs.aas.generated.model.AccessRequestStatusDto;
@@ -35,6 +40,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -63,6 +71,7 @@ public class AuthenticationFlowTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", containsString("/ssi/login")))
                 .andReturn();
+
         HttpSession session = result.getRequest().getSession(false);
 
         result = mockMvc.perform(
@@ -72,17 +81,23 @@ public class AuthenticationFlowTest {
                                 .session((MockHttpSession) session))
                 .andExpect(status().isOk())
                 .andReturn();
-        session = result.getRequest().getSession(false);
 
-        String qrUrl = (String) result.getModelAndView().getModel().get("qrUrl");
-        mockMvc.perform(
+        session = result.getRequest().getSession(false);
+        String qrUrl = result.getModelAndView().getModel().get("qrUrl").toString();
+        String userId = result.getModelAndView().getModel().get("requestId").toString();
+
+        result = mockMvc.perform(
                         get(qrUrl)
                                 .accept(MediaType.IMAGE_PNG, MediaType.IMAGE_GIF)
                                 .cookie(new Cookie("JSESSIONID", session.getId()))
                                 .session((MockHttpSession) session))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk()).andReturn();
 
-        String userId = (String) result.getModelAndView().getModel().get("requestId");
+        String expectedUrl = "uri://" + userId;
+        String resultQrUrl = decodeQR(result.getResponse().getContentAsByteArray());
+
+        assertEquals(expectedUrl, resultQrUrl);
+
         result = mockMvc.perform(
                         post("/login")
                                 .accept(MediaType.TEXT_HTML, MediaType.APPLICATION_XHTML_XML, MediaType.APPLICATION_XML)
@@ -93,6 +108,7 @@ public class AuthenticationFlowTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", containsString("/oauth2/authorize")))
                 .andReturn();
+
         session = result.getRequest().getSession(false);
 
         result = mockMvc.perform(
@@ -109,6 +125,7 @@ public class AuthenticationFlowTest {
         String reUrl = result.getResponse().getRedirectedUrl();
         MultiValueMap<String, String> params = UriComponentsBuilder.fromUriString(reUrl).build().getQueryParams();
         String code = params.getFirst("code");
+
         result = mockMvc.perform(
                         post("/oauth2/token")
                                 .header("Authorization", "Basic YWFzLWFwcC1vaWRjOnNlY3JldA==")
@@ -120,8 +137,8 @@ public class AuthenticationFlowTest {
                 .andReturn();
 
         String jwtStr = result.getResponse().getContentAsString();
-        JacksonJsonParser jsonParser = new JacksonJsonParser();
-        Map<String, Object> jwt = jsonParser.parseMap(jwtStr);
+        Map<String, Object> jwt = new JacksonJsonParser().parseMap(jwtStr);
+
         assertNotNull(jwt.get(OAuth2ParameterNames.ACCESS_TOKEN));
         assertNotNull(jwt.get(OidcParameterNames.ID_TOKEN));
         assertNotNull(jwt.get(OAuth2ParameterNames.EXPIRES_IN));
@@ -147,7 +164,8 @@ public class AuthenticationFlowTest {
                 .andReturn();
 
         session = result.getRequest().getSession(false);
-        result = mockMvc.perform(
+
+        mockMvc.perform(
                         get("/ssi/login")
                                 .accept(MediaType.TEXT_HTML, MediaType.APPLICATION_XHTML_XML, MediaType.APPLICATION_XML)
                                 .session((MockHttpSession) session))
@@ -179,6 +197,7 @@ public class AuthenticationFlowTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", containsString("/ssi/login")))
                 .andReturn();
+
         HttpSession session = result.getRequest().getSession(false);
 
         result = mockMvc.perform(
@@ -188,18 +207,28 @@ public class AuthenticationFlowTest {
                                 .session((MockHttpSession) session))
                 .andExpect(status().isOk())
                 .andReturn();
+
         session = result.getRequest().getSession(false);
+        String userId = result.getModelAndView().getModel().get("requestId").toString();
+        String qrUrl = result.getModelAndView().getModel().get("qrUrl").toString();
 
-        String qrUrl = (String) result.getModelAndView().getModel().get("qrUrl");
-
-        mockMvc.perform(
+        result = mockMvc.perform(
                         get(qrUrl)
                                 .accept(MediaType.IMAGE_PNG, MediaType.IMAGE_GIF)
                                 .cookie(new Cookie("JSESSIONID", session.getId()))
                                 .session((MockHttpSession) session))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk()).andReturn();
 
-        String userId = result.getModelAndView().getModel().get("requestId").toString();
+        String expectedUrl = "openid://" +
+                "?scope=openid" +
+                "&response_type=id_token" +
+                "&client_id=http://auth-server:9000" +
+                "&redirect_uri=http://auth-server:9000/ssi/siop-cb" +
+                "&response_mode=post" +
+                "&nonce=" + userId;
+        String resultQrUrl = decodeQR(result.getResponse().getContentAsByteArray());
+
+        assertEquals(expectedUrl, resultQrUrl);
 
         result = mockMvc.perform(
                         post("/login")
@@ -211,6 +240,7 @@ public class AuthenticationFlowTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", containsString("/oauth2/authorize")))
                 .andReturn();
+
         session = result.getRequest().getSession(false);
 
         result = mockMvc.perform(
@@ -227,6 +257,7 @@ public class AuthenticationFlowTest {
         String reUrl = result.getResponse().getRedirectedUrl();
         MultiValueMap<String, String> params = UriComponentsBuilder.fromUriString(reUrl).build().getQueryParams();
         String code = params.getFirst("code");
+
         result = mockMvc.perform(
                         post("/oauth2/token")
                                 .header("Authorization", "Basic YWFzLWFwcC1zaW9wOnNlY3JldDI=")
@@ -238,14 +269,20 @@ public class AuthenticationFlowTest {
                 .andReturn();
 
         String jwtStr = result.getResponse().getContentAsString();
-        JacksonJsonParser jsonParser = new JacksonJsonParser();
-        Map<String, Object> jwt = jsonParser.parseMap(jwtStr);
+        Map<String, Object> jwt = new JacksonJsonParser().parseMap(jwtStr);
+
         assertNotNull(jwt.get(OAuth2ParameterNames.ACCESS_TOKEN));
         assertNotNull(jwt.get(OidcParameterNames.ID_TOKEN));
         assertNotNull(jwt.get(OAuth2ParameterNames.EXPIRES_IN));
         assertNotNull(jwt.get(OAuth2ParameterNames.TOKEN_TYPE));
         assertTrue(((Integer) jwt.get(OAuth2ParameterNames.EXPIRES_IN)) > 500); // default is 300, we set it to 600
         assertEquals("Bearer", jwt.get(OAuth2ParameterNames.TOKEN_TYPE));
+    }
+
+    private String decodeQR(byte[] content) throws IOException, ChecksumException, NotFoundException, FormatException {
+        BufferedImage image = ImageIO.read(new ByteArrayInputStream(content));
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(image)));
+        return new QRCodeReader().decode(bitmap).getText();
     }
 
     @Test
