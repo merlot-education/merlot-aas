fce676a456a3d2e4825cc6f0db60c56ec873d3e6
Denis Sukhoroslov <denis.sukhoroslov@t-systems.com>
2022-08-30 17:45:21 +0300

    CORS disabled

--- service/src/main/java/eu/gaiax/difs/aas/config/SecurityConfig.java
+++ service/src/main/java/eu/gaiax/difs/aas/config/SecurityConfig.java
@@ -38,7 +38,7 @@ import eu.gaiax.difs.aas.service.SsiJwtCustomizer;
 /**
  * The Spring Security config.
  */
-@EnableWebSecurity //(debug = true)
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     private final String[] ANT_MATCHERS = {
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .cors().disable()
+                //.cors().disable()
                 .authorizeRequests()
                 .antMatchers(ANT_MATCHERS)
                 .permitAll()
